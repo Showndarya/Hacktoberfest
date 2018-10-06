508b356febe634ad2e60bef9e4d10566bb779f6a
Showndarya <showndarya.m@somaiya.edu>
2018-10-06 09:34:51 +0530

    Update Contributions page API URL

--- Contributors_Page_Source/index.js
+++ Contributors_Page_Source/index.js
@@ -2,37 +2,30 @@ var users = []
 var i = 0;
 
 var getData = (function($){
-	var URL = "https://api.github.com/repos/Showndarya/Hacktoberfest/commits?per_page=500"
+	var URL = "https://api.github.com/repos/Showndarya/Hacktoberfest/contributors"
 	
 	$.get(URL,function(data,status) {
 
 			data.forEach(function(d) {
 				
-				if(d.author==null){
+				if(d.login==null){
 					return true;
 				}
 				
-				if(users.indexOf(d.commit.author.email) == -1) {
-
-				    var mes = d.commit.message;
-				    var name = d.commit.author.name;
-				    
-				    if(name == ""){
-					    name = mes;
-				    }
-				    
+				if(users.indexOf(d.url) == -1) {
+	    
 				    var template = 
 				    "<div class='col-md-3 col-xs-6 col-lg-3' id='author'><div class='container-fluid'>" +
-					    "<a class='thumbnail' target='_blank' href='" + d.author.html_url + "'>" + 
-						    "<img src='" + d.author.avatar_url + "' alt='' class='img-responsive'>" +  
+					    "<a class='thumbnail' target='_blank' href='" + d.html_url + "'>" + 
+						    "<img src='" + d.avatar_url + "' alt='' class='img-responsive'>" +  
 					    "</a>" +
 					    "<div class='caption'>" +
-						    "<strong>" + d.author.login + "</strong>" +
-						    "<p>" + name + "</p>"+
+						    "<strong>" + d.login + "</strong>" +
+						    "<p> Contributions: " + d.contributions  + "</p>"+
 					    "</div></div></div>" 
 					    
 			        $('#contributors').append(template);
-			        users[i] = d.commit.author.email;
+			        users[i] = d.url;
 			        i=i+1;
                 }
                 
