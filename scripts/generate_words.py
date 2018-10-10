ecb380a4f54265fcb1af755056d1ed3d2f37ad98
hegde5 <vinayhegde2010@gmail.com>
2018-10-11 00:32:44 +0200

    Generate words with script

--- scripts/generate_words.py
+++ scripts/generate_words.py
@@ -1,3 +1,11 @@
+"""
+Helper script to generate JSON files in the required format
+
+Huge thanks to Datamuse for this cool API!
+Credits: Datamuse API
+API Reference: https://www.datamuse.com/api/
+"""
+
 import requests
 import os
 import json
@@ -12,31 +20,53 @@ PART_OF_SPEECH = {
 }
 
 
-def get_words_starting_with(letter, limit=10):
+def get_words_starting_with(letters, limit=10):
+    """
+    Gets words starting with the letters provided from datamuse API abd
+    creates the JSON file in the relevant directory.
 
+    :type letters: `str`
+    :param letters: can be a single letter or the starting letters of a word
+
+    :type limit: `int`
+    :param limit: number of words to be returned
+
+    :raises: Assertion Error if the limit is over 100
+
+    :rtype: None
+    """
     if limit > 100:
-        print ('Take it easy! Sorry, Setting the limit to 100.')
-        limit = 100
+        raise AssertionError('Take it easy! Sorry, Setting the limit to 100.')
 
     params = {
-        'sp': letter+'*',  # Spelled like
+        'sp': letters+'*',  # Spelled like
         'md': 'p,d',  # metadata part of speech, defn
         'max': limit
     }
     resp = requests.get(BASE_API, params=params)
     data = resp.json()
 
-    for data in resp:
-        _generate_json(data)
+    for entry in data:
+        _generate_json(entry)
 
 
 def _generate_json(data):
+    """
+    Generates the JSON in the required format
+    :type data: `dict`
+    :param data: data from datamuse API
 
-    if not data['tags']:
+    :rtype: None
+    """
+
+    if 'tags' not in data:
         return None
     elif data['tags'][0] not in PART_OF_SPEECH:
         return None
     else:
+        if 'defs' not in data:
+            return None
+
         pos = data['tags'][0]
         defns = data['defs']
         for defn in defns:
@@ -44,15 +74,25 @@ def _generate_json(data):
             pos_def = defn.split('\t')
             if pos_def and pos_def[0] in PART_OF_SPEECH:
                 word_json = {
-                    "word": data['word'],
+                    "word": data['word'].capitalize(),
                     "definitions": [pos_def[1]],
                     "parts-of-speech": PART_OF_SPEECH[pos]
                 }
-                _create_folder(word_json)
+                _create_file(word_json)
 
 
-def _create_folder(json_data):
+def _create_file(json_data):
+    """
+    Creates the JSON file in the relevant folder only if the file does
+    not exist already(Meaning, duplicate words are not allowed)
 
+    :type json_data: `dict`
+    :param json_data: data in the format to be written into the file
+
+    :raises: Assertion Error if the letter directory is not found
+
+    :rtype: None
+    """
     letter = json_data['word'][0].upper()
     current_path = os.path.dirname(os.path.realpath(__file__))
     parent_directory = os.path.abspath(os.path.join(current_path, os.pardir))
@@ -70,3 +110,16 @@ def _create_folder(json_data):
     else:
         raise AssertionError('Letter: {} directory not found'.format(letter))
 
+
+if __name__== "__main__":
+    """
+    Main to call to master function
+
+    Format to invoke the master func:
+        get_words_starting_with(some_letter_you)
+    
+    Example:
+        get_words_starting_with('ap')    
+    """
+
+    get_words_starting_with('Y', limit=20)
