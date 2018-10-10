64fe5e21513d9c18d05ad26ae1f2563db9b4d7eb
hegde5 <vinayhegde2010@gmail.com>
2018-10-10 23:52:30 +0200

    Add helper script

--- scripts/generate_words.py
+++ scripts/generate_words.py
@@ -1,4 +1,6 @@
 import requests
+import os
+import json
 
 
 BASE_API = 'https://api.datamuse.com/words/'
@@ -25,24 +27,46 @@ def get_words_starting_with(letter, limit=10):
     data = resp.json()
 
     for data in resp:
-        pass
+        _generate_json(data)
 
 
 def _generate_json(data):
 
     if not data['tags']:
         return None
-    elif data['tags'] > 1:
-        return None
     elif data['tags'][0] not in PART_OF_SPEECH:
         return None
     else:
         pos = data['tags'][0]
-        return {
-            "word": data['word'],
-            "definitions": [
-                "pleasing the senses or mind aesthetically",
-                "of a very high standard; excellent"
-            ],
-            "parts-of-speech": PART_OF_SPEECH[pos]
-        }
+        defns = data['defs']
+        for defn in defns:
+            # list consisting of part of speech and it's definition
+            pos_def = defn.split('\t')
+            if pos_def and pos_def[0] in PART_OF_SPEECH:
+                word_json = {
+                    "word": data['word'],
+                    "definitions": [pos_def[1]],
+                    "parts-of-speech": PART_OF_SPEECH[pos]
+                }
+                _create_folder(word_json)
+
+
+def _create_folder(json_data):
+
+    letter = json_data['word'][0].upper()
+    current_path = os.path.dirname(os.path.realpath(__file__))
+    parent_directory = os.path.abspath(os.path.join(current_path, os.pardir))
+
+    directory_entries = os.listdir('.')
+    if letter in directory_entries:
+        file_name = json_data['word'].capitalize() + '.json'
+        letter_file_path = os.path.join(parent_directory, letter)
+        path = os.path.join(letter_file_path, file_name)
+        letter_files = [letter_file.lower()
+                        for letter_file in os.listdir(letter_file_path)]
+        if file_name.lower() not in letter_files:
+            with open(path, 'w') as fp:
+                json.dump(json_data, fp)
+    else:
+        raise AssertionError('Letter: {} directory not found'.format(letter))
+
