240e6ee010c8f86be78c3b4d9770d131349f282a
Tanuj wagadre <47408779+i-tanuj@users.noreply.github.com>
2021-09-30 16:28:27 +0530

    update renamer

--- scripts/renamer.py
+++ scripts/renamer.py
@@ -7,5 +7,5 @@ for alphabet in dirs:
 	words_dir = project_path/alphabet
 	for json_file in words_dir.glob('*.json'):
 		if json_file.stem[0].islower():
-			json_file.rename(Path(json_file.parent,json_file.stem.capitalize()+'.json'))
+			json_file.rename(Path(json_file.parent,json_file.stem.capitalize()+".json"))
 			# print (json_file,json_file.stem.capitalize()+'.json')
